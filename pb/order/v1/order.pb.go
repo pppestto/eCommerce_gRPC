3a3c31b96777847f4b24b4f6d2a41e4bbe588cc2
pppestto <cnopcart@gmail.com>
2026-02-20 19:35:51 +0300

    chore: update userv1 path

--- pb/order/v1/order.pb.go
+++ pb/order/v1/order.pb.go
@@ -10,7 +10,7 @@ package v1
 
 import (
 	common "github.com/pppestto/ecommerce-grpc/pb/common"
-	v1 "github.com/pppestto/ecommerce-grpc/proto/gen/user/v1"
+	v1 "github.com/pppestto/ecommerce-grpc/pb/user/v1"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
